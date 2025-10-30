7c738abc195838cb7dffbc46af75d11318f771f3
Krishnapushpan <krishnapushpan2003@gmail.com>
2025-10-30 11:08:32 +0530

    updated

--- Frontend/src/pages/About.jsx
+++ Frontend/src/pages/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import girlImg from '../assets/girl2.jpg';
+import cvPdf from '../assets/KRISHNA_PUSHPAN.pdf';
 
 const About = () => (
   <section className="max-w-7xl mx-auto px-2 py-20 flex flex-col md:flex-row items-center gap-6">
@@ -8,7 +9,7 @@ const About = () => (
       <h2 className="text-4xl font-bold text-gray-800 mb-4">About Me</h2>
       <p className="text-xl text-gray-700 mb-4">Hi, I'm Krishna Pushpan. I'm a passionate MERN stack developer with a strong foundation in web development and blockchain. I love building dynamic web apps and learning new technologies. My goal is to create user-friendly solutions that make a difference.</p>
       <p className="text-gray-500 mb-6">Currently, I'm working as a MERN Stack Developer at the Incubation Center, Malabar College of Engineering and Technology, Thrissur. I enjoy working with React, Node.js, Mongodb and exploring new tech trends.</p>
-      <a href="/src/assets/KRISHNA_PUSHPAN.pdf" download className="bg-gray-900 text-white px-6 py-3 rounded-lg font-semibold shadow hover:bg-gray-800 transition inline-block">Download CV</a>
+      <a href={cvPdf} download className="bg-gray-900 text-white px-6 py-3 rounded-lg font-semibold shadow hover:bg-gray-800 transition inline-block">Download CV</a>
     </div>
     {/* Image */}
     <div className="flex-1 flex justify-center">
